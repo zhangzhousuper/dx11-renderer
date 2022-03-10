50d2c6b05c8c8bad4adf971692f1ddf11b284724
zhangzhousuper <1063052346@qq.com>
2022-03-10 23:51:38 +0800

    fix camera

--- dx11/GameApp.cpp
+++ dx11/GameApp.cpp
@@ -45,6 +45,39 @@ void GameApp::OnResize()
 
 	D3DApp::OnResize();
 
+	// 为D2D创建DXGI表面渲染目标
+	ComPtr<IDXGISurface> surface;
+	HR(m_pSwapChain->GetBuffer(0, __uuidof(IDXGISurface), reinterpret_cast<void**>(surface.GetAddressOf())));
+	D2D1_RENDER_TARGET_PROPERTIES props = D2D1::RenderTargetProperties(
+		D2D1_RENDER_TARGET_TYPE_DEFAULT,
+		D2D1::PixelFormat(DXGI_FORMAT_UNKNOWN, D2D1_ALPHA_MODE_PREMULTIPLIED));
+	HRESULT hr = m_pd2dFactory->CreateDxgiSurfaceRenderTarget(surface.Get(), &props, m_pd2dRenderTarget.GetAddressOf());
+	surface.Reset();
+
+	if (hr == E_NOINTERFACE)
+	{
+		OutputDebugStringW(L"\n警告：Direct2D与Direct3D互操作性功能受限，你将无法看到文本信息。现提供下述可选方法：\n"
+			L"1. 对于Win7系统，需要更新至Win7 SP1，并安装KB2670838补丁以支持Direct2D显示。\n"
+			L"2. 自行完成Direct3D 10.1与Direct2D的交互。详情参阅："
+			L"https://docs.microsoft.com/zh-cn/windows/desktop/Direct2D/direct2d-and-direct3d-interoperation-overview""\n"
+			L"3. 使用别的字体库，比如FreeType。\n\n");
+	}
+	else if (hr == S_OK)
+	{
+		// 创建固定颜色刷和文本格式
+		HR(m_pd2dRenderTarget->CreateSolidColorBrush(
+			D2D1::ColorF(D2D1::ColorF::White),
+			m_pColorBrush.GetAddressOf()));
+		HR(m_pdwriteFactory->CreateTextFormat(L"宋体", nullptr, DWRITE_FONT_WEIGHT_NORMAL,
+			DWRITE_FONT_STYLE_NORMAL, DWRITE_FONT_STRETCH_NORMAL, 15, L"zh-cn",
+			m_pTextFormat.GetAddressOf()));
+	}
+	else
+	{
+		// 报告异常问题
+		assert(m_pd2dRenderTarget);
+	}
+
 	// 摄像机变更显示
 	if (m_pCamera != nullptr)
 	{
