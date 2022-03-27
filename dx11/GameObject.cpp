f05af0bd266994c4643d6521be3a59416fc91029
zhangzhousuper <1063052346@qq.com>
2022-03-28 01:13:50 +0800

    cannot load house..

--- dx11/GameObject.cpp
+++ dx11/GameObject.cpp
@@ -84,6 +84,10 @@ void GameObject::Draw(ID3D11DeviceContext * deviceContext, IEffect* effect)
 	for (auto& part : m_Model.modelParts)
 	{
 		// 设置顶点/索引缓冲区
+		deviceContext->IASetVertexBuffers(0, 1, part.vertexBuffer.GetAddressOf(), &strides, &offsets);
+		deviceContext->IASetIndexBuffer(part.indexBuffer.Get(), part.indexFormat, 0);
+		
+		// 更新数据并应用
 		IEffectTransform* pEffectTransform = dynamic_cast<IEffectTransform*>(effect);
 		if (pEffectTransform)
 		{
